e027ef2e1868fd8dee2e5d0a6e3dd5824abddd2c
Marcos Vicente <marcosvicentesiqueira@gmail.com>
2024-01-22 06:49:02 -0300

    proponent controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'
+
+  resources :proponents
+  root 'proponents#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Inss.find_or_create_by!(
+  from: 1,
+  to: 1045.00,
+  aliquot: 7.5
+)
+Inss.find_or_create_by!(
+  from: 1045.01,
+  to: 2089.60,
+  aliquot: 9
+)
+Inss.find_or_create_by!(
+  from: 2089.61,
+  to: 3134.40,
+  aliquot: 12
+)
+Inss.find_or_create_by!(
+  from: 3134.41,
+  to: 6101.06,
+  aliquot: 14
+)
+
+10.times do |i|
+  FactoryBot.create(:proponent)
+end
